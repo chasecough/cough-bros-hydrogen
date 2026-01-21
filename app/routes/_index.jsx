5c788e38d3a3cb8375bd1772a78584604b49fe3f
chasecough <chaselindsay22129@gmail.com>
2026-01-20 19:57:40 -0600

    Update _index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,4 @@
-import {Await, useLoaderData, Link} from 'react-router';
+<h1>Cough Bros Inc. — Grow Smarter</h1>import {Await, useLoaderData, Link} from 'react-router';
 import {Suspense} from 'react';
 import {Image} from '@shopify/hydrogen';
 import {ProductItem} from '~/components/ProductItem';
